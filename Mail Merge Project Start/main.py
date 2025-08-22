ac00ffcde44e0de9897caf9bfb19e96b0020539e
TechWriterP <techwriter.prashant@gmail.com>
2025-08-22 08:50:01 +0530

    Day-24 learnings

---
+++ Mail Merge Project Start/main.py
@@ -0,0 +1,35 @@
+
+PLACEHOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.docx", mode="w") as final_letter:
+            final_letter.write(new_letter)
+
+
+
+
+
+
+#TODO: Create a letter using starting_letter.txt
+
+#
+# with open("./Input/Names/invited_names.txt") as f:
+#     for x in f:
+#         new_inv = f"{x}.txt"
+#         print(new_inv)
+
+
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
